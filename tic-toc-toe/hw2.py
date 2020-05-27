c2e15b4dc6395e64ecb7a78d37feb455db360166
divyeshds2012 <divyeshds2012@gmail.com>
2020-05-27 15:54:18 +0530

    su kevu have

--- tic-toc-toe/hw2.py
+++ tic-toc-toe/hw2.py
@@ -1,7 +1,7 @@
 # make tic-toc-toe
 
-# this is game now
- #   ds215 
+# this is game now and lets try to understand git
+
 #Tic-tac-toe (or noughts and crosses) is a simple strategy game in which two players take turns placing a mark on a 3x3 board, attempting to make a row, column, or diagonal of three with their mark. In this homework, we will use the tools we've covered in the past two weeks to create a tic-tac-toe simulator and evaluate basic winning strategies.
 import numpy as np
 import random
